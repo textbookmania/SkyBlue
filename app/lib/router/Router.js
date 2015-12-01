f2df5aeff16199d6bbf77384ba5ee77972b67336
Kelsie Yamakawa <kayama@hawaii.edu>
2015-11-30 15:39:06 -1000

    Help page

--- app/lib/router/Router.js
+++ app/lib/router/Router.js
@@ -3,6 +3,19 @@
  * See: http://iron-meteor.github.io/iron-router/
  */
 
+/**
+ * This function returns the username of the user
+ * before getting the data of the user's profile.
+ * @returns profile name of user
+ */
+checkUsername = function() {
+  if (Meteor.user()) {
+    if (!Meteor.loggingIn()) {
+      return Meteor.user().profile.name;
+    }
+  }
+}
+
 
 Router.configure({
   layoutTemplate: 'Layout',
@@ -10,8 +23,18 @@ Router.configure({
   loadingTemplate: 'Loading'
 });
 
+Router.configure({
+  layoutTemplate: 'Layout',
+  waitOn: function() { return Meteor.subscribe("Student"); },
+  loadingTemplate: 'Loading'
+});
+
 Router.route('/', {
   name: 'Home'
+});
+
+Router.route('/help', {
+  name: 'Help'
 });
 
 Router.route('/listtextbook', {
@@ -26,11 +49,24 @@ Router.route('/add', {
   name: 'AddBuyOffer'
 });
 
-Router.route('buyoffer', {
+Router.route('/buyoffer', {
   name: 'BuyOffer'
+});
+
+Router.route('/liststudent', {
+  name: 'ListStudent'
+});
+
+Router.route('/addstudent', {
+  name: 'AddStudent'
 });
 
 Router.route('/stuff/:_id', {
   name: 'EditBuyOffer',
   data: function() { return BuyOffer.findOne(this.params._id); }
+});
+
+Router.route('/student/', {
+  name: 'EditStudent',
+  data: function() { return Student.findOne({email: checkUsername()}) }
 });
