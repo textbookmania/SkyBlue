782debed0a7ec5b24fcfae5372eeb0418ac33cdd
Aljon Preza <aljonpreza@yahoo.com>
2015-12-03 09:28:30 -1000

    Implemented View Offer

--- app/client/templates/pages/View Offers/ViewYourOffers.js
+++ app/client/templates/pages/View Offers/ViewYourOffers.js
@@ -6,10 +6,10 @@ Template.ViewYourOffers.helpers({
    * @returns {*} All of the Contact documents.
    */
   viewBuyOffers: function () {
-    return BuyOffer.find();
+    return BuyOffer.find({studentID: Meteor.user().profile.name});
   },
 
   viewSellOffers: function () {
-    return _.where(SellOffer, {studentID: Meteor.user().profile.name});;
+    return SellOffer.find({studentID: Meteor.user().profile.name})
   }
 });
