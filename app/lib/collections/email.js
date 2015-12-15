55c848001863784c6959f794610b8e5f154808fe
khongnaw <android.prok@gmail.com>
2015-12-14 20:41:34 -1000

    add email template

--- app/lib/collections/email.js
+++ app/lib/collections/email.js
@@ -5,7 +5,7 @@
 
 if(Meteor.isServer){
     Meteor.startup(function(){
-        process.env.MAIL_URL = 'smptp://postmaster%40sandboxdcd4f48ef10f49fcb182f81c88329310.mailgun.org:9e8c91e97978835bc5b72541b0ba90d1'
+        process.env.MAIL_URL = 'smptp://postmaster%40sandboxdcd4f48ef10f49fcb182f81c88329310.mailgun.org:9e8c91e97978835bc5b72541b0ba90d1@smtp.mailgun.org'
         
     });
 }
