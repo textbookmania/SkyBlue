721604d1014a3528e135a7bd35d361962fe70559
khongnaw <android.prok@gmail.com>
2015-12-14 23:40:08 -1000

    added ViewAcceptedOffers

---
+++ app/client/templates/pages/View Offers Accepted/ViewAcceptedOffers.js
@@ -0,0 +1,60 @@
+/**
+ * Created by sorap on 12/14/2015.
+ */
+Template.ViewAcceptedOffers.helpers({
+
+    /**
+     * @returns {*} All of the Contact documents.
+     */
+    viewBuyOffers: function () {
+        return BuyOffer.find({studentID: Meteor.user().profile.name});
+    },
+
+    viewSellOffers: function () {
+        return SellOffer.find({studentID: Meteor.user().profile.name})
+    },
+
+    formatDate: function (date) {
+        var currDate = new Date();
+        var newDate = moment(date).format('ll');
+        if (date <= currDate) {
+            newDate = newDate.fontcolor("red");
+            return newDate;
+        }
+
+        return newDate;
+    },
+
+    formatTime: function (date) {
+        var currDate = new Date();
+        var newDate = moment(date).format('h:mm a');
+        if (date <= currDate) {
+            newDate = newDate.fontcolor("red");
+            return newDate;
+        }
+
+        return newDate;
+    }
+});
+
+Template.ViewYourOffers.events({
+    'click .deletebuy': function(e) {
+        e.preventDefault();
+
+        if (confirm("Delete this post?")) {
+            var currentPostId = this._id;
+            Meteor.call("deleteBuyOffer", currentPostId);
+            Router.go('ViewYourOffers');
+        }
+    },
+
+    'click .deletesell': function(e) {
+        e.preventDefault();
+
+        if (confirm("Delete this post?")) {
+            var currentPostId = this._id;
+            Meteor.call("deleteSellOffer", currentPostId);
+            Router.go('ViewYourOffers');
+        }
+    }
+});
