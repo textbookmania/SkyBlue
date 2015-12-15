812cf39063eff9247fb06889be804c8426d396d2
khongnaw <android.prok@gmail.com>
2015-12-14 19:13:47 -1000

    emailTest

---
+++ app/lib/collections/email.js
@@ -0,0 +1,10 @@
+/**
+ * Created by sorap on 12/14/2015.
+ */
+
+
+if(Meteor.isServer){
+    Meteor.startup(function(){
+        process.env.MAIL_URL = 'smptp://postmaster%40sandboxdcd4f48ef10f49fcb182f81c88329310.mailgun.org:9e8c91e97978835bc5b72541b0ba90d1'
+    });
+}
