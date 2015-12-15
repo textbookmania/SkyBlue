d2b1d80de50ebf319c9b23119bdd445d91aed90d
khongnaw <android.prok@gmail.com>
2015-12-14 19:34:57 -1000

    fixed error

--- app/lib/collections/email.js
+++ app/lib/collections/email.js
@@ -6,5 +6,6 @@
 if(Meteor.isServer){
     Meteor.startup(function(){
         process.env.MAIL_URL = 'smptp://postmaster%40sandboxdcd4f48ef10f49fcb182f81c88329310.mailgun.org:9e8c91e97978835bc5b72541b0ba90d1'
+        
     });
 }
