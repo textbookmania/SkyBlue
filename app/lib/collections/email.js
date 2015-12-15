789ea9f53d7f204cfe193403607c7ca6a33c9993
khongnaw <android.prok@gmail.com>
2015-12-14 20:50:34 -1000

    added Meteor method for email

--- app/lib/collections/email.js
+++ app/lib/collections/email.js
@@ -2,10 +2,37 @@
  * Created by sorap on 12/14/2015.
  */
 
+if(Meteor.isClient){
+    Meteor.call('sendEmail',
+        'Android.prok@gmail.com',
+        'bob@example.com',
+        'Hello from Meteor!',
+        'This is a test of Email.send.');
+
+    console.log("email sent");
+}
+
 
 if(Meteor.isServer){
     Meteor.startup(function(){
-        process.env.MAIL_URL = 'smptp://postmaster%40sandboxdcd4f48ef10f49fcb182f81c88329310.mailgun.org:9e8c91e97978835bc5b72541b0ba90d1@smtp.mailgun.org'
-        
+        process.env.MAIL_URL = 'smtp://postmaster%40sandboxdcd4f48ef10f49fcb182f81c88329310.mailgun.org:9e8c91e97978835bc5b72541b0ba90d1@smtp.mailgun.org'
+    });
+
+
+    Meteor.methods({
+        sendEmail: function (to, from, subject, text) {
+            check([to, from, subject, text], [String]);
+
+            // Let other method calls from the same client start running,
+            // without waiting for the email sending to complete.
+            this.unblock();
+
+            Email.send({
+                to: to,
+                from: from,
+                subject: subject,
+                text: text
+            });
+        }
     });
 }
